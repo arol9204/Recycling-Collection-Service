238d229f1cf8a985d3864fe263248848622cbb31
Alejandro Rodriguez <63767771+arol9204@users.noreply.github.com>
2023-06-29 09:44:53 -0400

    Update app.py

--- Shinyapp/app.py
+++ Shinyapp/app.py
@@ -1,20 +1,52 @@
 
 import numpy as np
-import skimage
-from PIL import Image, ImageOps
-from shiny import App, Inputs, Outputs, Session, render, ui
+#import skimage
+import matplotlib.pyplot as plt
+#from PIL import Image, ImageOps
+from shiny import App, Inputs, Outputs, Session, reactive, render, ui
 from shiny.types import FileInfo, ImgData, SilentException
 
 
-# theme
+# Request tab
+import tensorflow as tf
 
+# Recreate the exact same model, including its weights and the optimizer
+new_model = tf.keras.models.load_model('model_2_94p.h5')
+
+def predictions(image):
+     img_array = tf.keras.preprocessing.image.img_to_array(image) # convert the image to an NumPy array
+     img_array_resized = tf.keras.preprocessing.image.smart_resize(img_array, size = (128, 128)) / 255.0 # if necessary, resize the image to 128 by 128
+     reshaped_image = np.expand_dims(img_array_resized, axis=0) # reshape the image from (height, width, 3) to (1, height, width, 3) as an input to the CNN model
+     probabilities = new_model.predict(reshaped_image)
+     probabilities = np.round(probabilities[0,:],4 ) * 100
+     return probabilities
+
+
+
+# Map tab
+from shiny import *
+from shinywidgets import output_widget, render_widget
+import ipyleaflet as L
+
+basemaps = {
+  "OpenStreetMap": L.basemaps.OpenStreetMap.Mapnik,
+  "Stamen.Toner": L.basemaps.Stamen.Toner,
+  "Stamen.Terrain": L.basemaps.Stamen.Terrain,
+  "Stamen.Watercolor": L.basemaps.Stamen.Watercolor,
+  "Satellite": L.basemaps.Gaode.Satellite,
+}
+
+
+
+# theme
+import shinyswatch
 
 app_ui = ui.page_navbar(
     # Available themes:
-    #  cerulean, cosmo, cyborg, darkly, flatly, journal, litera, lumen, lux,
-    #  materia, minty, morph, pulse, quartz, sandstone, simplex, sketchy, slate,
-    #  solar, spacelab, superhero, united, vapor, yeti, zephyr
-
+    #  cerulean, cosmo, cyborg, darkly, flatly*, journal, litera, lumen, lux,
+    #  materia, minty, morph, pulse, quartz, sandstone, simplex, sketchy, slate*,
+    #  solar, spacelab*, superhero, united, vapor, yeti, zephyr*
+    shinyswatch.theme.zephyr(),
     
 
     # Land tab App information -----------------------------
@@ -25,8 +57,7 @@ app_ui = ui.page_navbar(
     ui.nav("Request Submission", 
                    ui.layout_sidebar(
                        ui.panel_sidebar(
-                                        ui.input_file(
-                                                        "file",
+                                        ui.input_file(  "file",
                                                         "Please upload your picture here",
                                                         button_label="Open camera",
                                                         # This tells it to accept still photos only (not videos).
@@ -36,19 +67,25 @@ app_ui = ui.page_navbar(
                                                         ),
                                         ui.output_image("image"),
                                        ),
-                       ui.panel_main(),
+                       ui.panel_main(
+                                        ui.output_plot("plot1", click=True, dblclick=True, hover=True, brush=True),
+                                    ),
                    ),
           ),
 
     # Map for exploring requests ---------------------------
-    ui.nav("Map"),
-    
-    
+    ui.nav("Map",
+             
+                            ui.input_select(
+                                            "basemap", "Choose a basemap",
+                                            choices=list(basemaps.keys())
+                                            ),
+                            output_widget("map")
+             
+          ),
     
     title="RecyclingMates",
 )
-
-
 
 def server(input: Inputs, output: Outputs, session: Session):
     @output
@@ -59,32 +96,38 @@ def server(input: Inputs, output: Outputs, session: Session):
             raise SilentException()
 
         file_info = file_infos[0]
-        img = Image.open(file_info["datapath"])
+        img: ImgData = {"src": str(file_info["datapath"]), "width": "300px"}
+        return img
+    
 
-        # # Resize to 1000 pixels wide
-        # basewidth = 1000
-        # wpercent = basewidth / float(img.size[0])
-        # hsize = int((float(img.size[1]) * float(wpercent)))
-        # img = img.resize((basewidth, hsize), Image.ANTIALIAS)
+    # request tab plot
+    @output
+    @render.plot()
+    def plot1():
+        file_infos: list[FileInfo] = input.file()
+        if not file_infos:
+            raise SilentException()
 
-        # # Convert to grayscale
-        # img = ImageOps.grayscale(img)
+        file_info = file_infos[0]
+        img = tf.keras.preprocessing.image.load_img(file_info["datapath"])
+        p = predictions(img)
+        x = np.array([0, 1, 2, 3])  # X-axis values
+        labels = ['Can', 'Cardboard', 'Glass bottle', 'Plastic Bottle']  # Replace with your labels
+        fig = plt.bar(x, p)
+        fig = plt.xticks(x, labels)  # Assigning labels to x-axis ticks
+        fig = plt.xlabel('Type of Object')
+        fig = plt.ylabel('Probability')
+        fig = plt.title('CNN Model Predictions')
+        for i, value in enumerate(p):
+           fig = plt.text(i, value, str(value) + "%", ha='center', va='bottom')
+        return fig
 
-        # # Rotate image based on EXIF tag
-        # img = ImageOps.exif_transpose(img)
-
-        # # Convert to numpy array for skimage processing
-        # image_data = np.array(img)
-
-        # # Apply thresholding
-        # val = skimage.filters.threshold_otsu(image_data)
-        # mask = image_data < val
-
-        # Save for render.image
-        skimage.io.imsave("small.png", skimage.util.img_as_ubyte(img))
-        return {"src": "small.png", "width": "100%"}
-
-
+    # Map Tab
+    @output 
+    @render_widget
+    def map():
+        basemap = basemaps[input.basemap()]
+        return L.Map(basemap=basemap, center=[42.297471, -83.008058], zoom=9)
 
 
 app = App(app_ui, server)
