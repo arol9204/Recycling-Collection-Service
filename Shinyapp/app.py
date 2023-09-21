e94be8384a30581398ba6f875ce459ea18b95406
Alejandro <63767771+arol9204@users.noreply.github.com>
2023-09-21 10:25:29 -0400

    App update with Roboflow intagration.

--- Shinyapp/app.py
+++ Shinyapp/app.py
@@ -1,23 +1,29 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 from shiny import App, Inputs, Outputs, Session, reactive, render, ui
 from shiny.types import FileInfo, ImgData, SilentException
 #import requests
 import exifread # to extract image metadata
 
-# Request tab
-import tensorflow as tf
+import cv2
 
-# Loading the model trainied
-new_model = tf.keras.models.load_model('model_2_94p.h5')
+# importing the model from Robolow
+from roboflow import Roboflow
+rf = Roboflow(api_key="F7o8gC2NLhuzMSLzk98A")
+project = rf.workspace().project("recycling-objects-4aqr3")
+model = project.version(1).model
 
-def predictions(image):
-     img_array = tf.keras.preprocessing.image.img_to_array(image) # convert the image to an NumPy array
-     img_array_resized = tf.keras.preprocessing.image.smart_resize(img_array, size = (128, 128)) / 255.0 # if necessary, resize the image to 128 by 128
-     reshaped_image = np.expand_dims(img_array_resized, axis=0) # reshape the image from (height, width, 3) to (1, height, width, 3) as an input to the CNN model
-     probabilities = new_model.predict(reshaped_image)
-     probabilities = np.round(probabilities[0,:],4 ) * 100
-     return probabilities
+
+from geopy.geocoders import Nominatim
+
+# Create a Nominatim geocoder
+geolocator = Nominatim(user_agent="reverse_geocoding")
+
+
+
+
 
 # Defining the function to extract the metadata of the image
 def get_gps_info(image):
@@ -42,12 +48,18 @@ def get_gps_info(image):
 
         latitude = lat_deg + (lat_min / 60.0) + (lat_sec / 3600.0)
         longitude = lon_deg + (lon_min / 60.0) + (lon_sec / 3600.0)
-    
-    if tags['GPS GPSLongitudeRef'].values == 'W':
+        if tags['GPS GPSLongitudeRef'].values == 'W':
             longitude *= -1  # Convert to negative longitude
+    else:
+        latitude = 'NA'
+        longitude = 'NA'
+    
+    
 
     if 'EXIF DateTimeOriginal' in tags:
         timestamp = tags['EXIF DateTimeOriginal'].values
+    else:
+        timestamp = 'NA'
 
     return latitude, longitude, timestamp
 
@@ -68,6 +80,14 @@ basemaps = {
 
 # theme
 import shinyswatch
+
+
+
+
+
+###############################################
+# ------------     UI     ------------------- #
+###############################################
 
 app_ui = ui.page_navbar(
     # Available themes:
@@ -93,13 +113,37 @@ app_ui = ui.page_navbar(
                                                         capture="environment",
                                                         ),
                                         ui.output_image("image"),
-                                        ui.output_text_verbatim("lat"),
-                                        ui.output_text_verbatim("lon"),
+                                        ui.input_action_button("detect", "Detect!"),
+
+                                        
+
                                        ),
                        ui.panel_main(
-                                        ui.output_plot("plot1", click=True, dblclick=True, hover=True, brush=True),
+                                        ui.output_plot("image_p"),
+                                        
+                                        ui.row(
+                                                ui.column(4, "# Cans "),
+                                                ui.column(4, "# Glass bottle"),
+                                                ui.column(4, "# Plastic bottle"),
+                                              ),
+                                        ui.row(
+                                                ui.column(4, ui.output_text_verbatim("can", placeholder=True)),
+                                                ui.column(4, ui.output_text_verbatim("glass_bottle", placeholder=True)),
+                                                ui.column(4, ui.output_text_verbatim("plastic_bottle", placeholder=True)),
+                                              ),
+
+                                        ui.row(
+                                                ui.column(6, "Latitude"),
+                                                ui.column(6, "Longitude"),
+                                              ),
+                                        
+                                        ui.row(
+                                                ui.column(6, ui.output_text_verbatim("lat", placeholder=True)),
+                                                ui.column(6, ui.output_text_verbatim("lon", placeholder=True)),
+                                              )
                                     ),
                    ),
+                   ui.input_action_button("submit", "Submit Request!"),
           ),
 
     # Map for exploring requests ---------------------------
@@ -110,10 +154,21 @@ app_ui = ui.page_navbar(
                                             choices=list(basemaps.keys())
                                             ),
                             output_widget("map"),
+                            
+                            ui.output_table("requests"),
           ),
     
-    title="RecyclingMates",
+    title="Recycling Service Request",
 )
+
+
+
+
+
+###############################################
+# ------------     SERVER ------------------- #
+###############################################
+
 
 def server(input: Inputs, output: Outputs, session: Session):
     @output
@@ -127,63 +182,240 @@ def server(input: Inputs, output: Outputs, session: Session):
         img: ImgData = {"src": str(file_info["datapath"]), "width": "300px"}
         return img
     
-    # request tab plot
-    @output
-    @render.plot()
-    def plot1():
+    
+    @reactive.Calc
+    def predictions():
         file_infos: list[FileInfo] = input.file()
         if not file_infos:
             raise SilentException()
 
-        file_info = file_infos[0]
-        img = tf.keras.preprocessing.image.load_img(file_info["datapath"])
-        p = predictions(img)
-        x = np.array([0, 1, 2, 3])  # X-axis values
-        labels = ['Can', 'Cardboard', 'Glass bottle', 'Plastic Bottle']  # Replace with your labels
-        fig = plt.bar(x, p)
-        fig = plt.xticks(x, labels)  # Assigning labels to x-axis ticks
-        fig = plt.xlabel('Type of Object')
-        fig = plt.ylabel('Probability')
-        fig = plt.title('CNN Model Predictions \n *The class with the highest probability is the object class identified by the model in the picture.')
-        for i, value in enumerate(p):
-           fig = plt.text(i, value, str(value) + "%", ha='center', va='bottom')
-        return fig
+        path = file_infos[0]['datapath']
 
-    
-    
-    @output
-    @render.text
-    def lat():
+        input.detect()
+        with reactive.isolate():
+            predictions = model.predict(path, confidence=40, overlap=30)
+                 
+            return predictions
+        
+    # Here we count all the recyling objects in the image
+    @reactive.Calc
+    def classes():
+
+        input.detect()
+        with reactive.isolate():
+            json_predictions = predictions().json()
+            l_classes = []
+            for i in json_predictions['predictions']:
+                l_classes.append(i['class'])
+
+        
+            return l_classes
+        
+    @reactive.Calc
+    def image_path():
         file_infos: list[FileInfo] = input.file()
+        
+        # If there is NOT GPS information it should raise an exception message
         if not file_infos:
             raise SilentException()
         file_info = file_infos[0]["datapath"]
-        latitude = get_gps_info(file_info)[0]
-        return f"Latitude: {latitude}"
+        return file_info
+
+    @reactive.Calc
+    def image_anotations():
+
+        image = mpimg.imread(image_path())
+       
+        # infer on a local image
+        json_predictions = predictions().json()
+
+        for bounding_box in json_predictions['predictions']:
+            x0 = bounding_box['x'] - bounding_box['width'] / 2
+            x1 = bounding_box['x'] + bounding_box['width'] / 2
+            y0 = bounding_box['y'] - bounding_box['height'] / 2
+            y1 = bounding_box['y'] + bounding_box['height'] / 2
+    
+            start_point = (int(x0), int(y0))
+            end_point = (int(x1), int(y1))
+
+            cv2.rectangle(image, start_point, end_point, color=(255,0,0), thickness=5)
+    
+            cv2.putText(
+                        image,
+                        bounding_box["class"],
+                        (int(x0), int(y0) - 10),
+                        fontFace = cv2.FONT_HERSHEY_SIMPLEX,
+                        fontScale = 1.5,
+                        color = (255, 0, 0),
+                        thickness=3
+                        )
+        
+        return image
+
+    # Here we show the image with the annotations
+    @output
+    @render.plot()
+    def image_p():
+        input.detect()
+        with reactive.isolate():
+            return plt.imshow(image_anotations())
+
+    
+    @output
+    @render.text()
+    def can():
+        input.detect()
+        with reactive.isolate():
+            return np.count_nonzero(np.array(classes()) == 'can')
+    
+    @output
+    @render.text()
+    def glass_bottle():
+        input.detect()
+        with reactive.isolate():
+            return np.count_nonzero(np.array(classes()) == 'glass bottle')
+    
+    @output
+    @render.text()
+    def plastic_bottle():
+        input.detect()
+        with reactive.isolate():
+            return np.count_nonzero(np.array(classes()) == 'plastic bottle')
+    
+
+    @output
+    @render.text
+    def lat():
+        input.detect()
+        with reactive.isolate():
+            latitude = get_gps_info(image_path())[0]
+        
+            return latitude
     
     @output
     @render.text
     def lon():
-        file_infos: list[FileInfo] = input.file()
-        if not file_infos:
-            raise SilentException()
-        file_info = file_infos[0]["datapath"]
-        longitude = get_gps_info(file_info)[1]
-        return f"Longitude: {longitude}"
+        input.detect()
+        with reactive.isolate():
+            longitude = get_gps_info(image_path())[1]
+            return longitude
     
-    # Map Tab
+    # Map Tab ---------------------------------------------------------------------
     @output 
     @render_widget
     def map():
+        # Now that we have the image information we can plot the map with the markers
         basemap = basemaps[input.basemap()]
-        m = L.Map(basemap=basemap, center=[42, -83], zoom=9)
-        marker = L.Marker(location=[42.31253333333333, -83.04131944444444], draggable=True)
+        m = L.Map(basemap=basemap, center=[get_gps_info(image_path())[0], get_gps_info(image_path())[1]], zoom=15)
+
+        # Here is the lat and lon of the image that will be upload
+        marker = L.Marker(location=[get_gps_info(image_path())[0], get_gps_info(image_path())[1]], draggable=True)
+
+        # Beer stores markers
         icon = Icon(icon_url='https://leafletjs.com/examples/custom-icons/leaf-green.png', icon_size=[20, 50], icon_anchor=[22,94])
-        beer_store_mark1 = L.Marker(location=[42.31263551985872, -83.03326561020128], icon=icon, rotation_angle=90, rotation_origin='22px 94px', draggable=False)
-        beer_store_mark2 = L.Marker(location=[42.30366417918876, -83.05465990194318], icon=icon, rotation_angle=90, rotation_origin='22px 94px', draggable=False)
+        beer_store_mark1 = L.Marker(location=[42.31263551985872, -83.03326561020128], icon=icon, rotation_angle=0, rotation_origin='22px 94px', draggable=False)
+        beer_store_mark2 = L.Marker(location=[42.30366417918876, -83.05465990194318], icon=icon, rotation_angle=0, rotation_origin='22px 94px', draggable=False)
+
+
+        # Adding the last request    
         m.add_layer(marker)
+
+        # Adding all previous requests:
+        # Create markers from the DataFrame
+        for index, row in df().iloc[:-1].iterrows():
+            circle_marker = L.CircleMarker(location=(row['latitude'], row['longitude']), radius=5, color="blue", fill_color="blue")
+            m.add_layer(circle_marker)
+
         m.add_layer(beer_store_mark1)
         m.add_layer(beer_store_mark2)
         return m
+    
+    # Data Frame requests ---------------------------------------------------------
+    # Here we define all the reactive values
+    request_id_col = reactive.Value([])
+    date_col = reactive.Value([])
+
+    cans_col = reactive.Value([])
+    glass_bottles_col = reactive.Value([])
+    plastic_bottle_col = reactive.Value([])
+    lat_col = reactive.Value([])
+    long_col = reactive.Value([])
+    city_col = reactive.Value([])
+    province_col = reactive.Value([])
+    country_col = reactive.Value([])
+
+    df=reactive.Value(pd.DataFrame({'request_ID': [],
+                                    'date': [],
+                                   'cans': [],
+                                   'glass_bottles': [],
+                                   'plastic_bottles': [],
+                                   'latitude': [],
+                                   'longitude': [],
+                                   'city': [],
+                                   'province': [],
+                                   'country': []
+                                   
+                                   }))
+
+    @reactive.Effect
+    @reactive.event(input.submit)
+    def add_value_to_list():
+        request_id_col.set(request_id_col() + [len(request_id_col()) + 1])
+        date_col.set(date_col() + [get_gps_info(image_path())[2]])
+
+        cans_col.set(cans_col() + [np.count_nonzero(np.array(classes()) == 'can')])
+        glass_bottles_col.set(glass_bottles_col() + [np.count_nonzero(np.array(classes()) == 'glass bottle')])
+        plastic_bottle_col.set(plastic_bottle_col() + [np.count_nonzero(np.array(classes()) == 'plastic bottle')])
+
+        lat_col.set(lat_col() + [get_gps_info(image_path())[0]])
+        long_col.set(long_col() + [get_gps_info(image_path())[1]])
+
+        #latitude = get_gps_info(image_path())[0]
+        #longitude = get_gps_info(image_path())[1]
+        
+        # Perform reverse geocoding
+        location = geolocator.reverse(f"{get_gps_info(image_path())[0]}, {get_gps_info(image_path())[1]}", exactly_one=True)
+        
+        # Extract city, province, and country
+        if location:
+            address = location.raw['address']
+            city = address.get('city', '')
+            province = address.get('state', '')
+            country = address.get('country', '')
+        else:
+            city = province = country = 'Unknown'
+        
+        city_col.set(city_col() + [city])
+        province_col.set(province_col() + [province])
+        country_col.set(country_col() + [country])
+        
+
+        new_data = {'request_ID': request_id_col(),
+                    'date': date_col(),
+                     'cans': cans_col(), 
+                     'glass_bottles': glass_bottles_col(),
+                     'plastic_bottles': plastic_bottle_col(),
+                     'latitude': lat_col(),
+                     'longitude': long_col(),
+                     'city': city_col(),
+                     'province': province_col(),
+                     'country': country_col()
+
+
+                     }
+        df.set(pd.DataFrame(new_data))
+        
+
+  
+    @output
+    @render.table
+    def requests():
+        #df = df.append(new_request(), ignore_index=True)
+        return df()
 
 app = App(app_ui, server)
+
+
+
+# When the image do not have lat and long it throw and error
+# When an object wasn't detected it is necesary to pop up a message saying that it happened and allow the user to select if it was an error or not, also i case that it was true that there is not a recycling object do not sumbit a request
